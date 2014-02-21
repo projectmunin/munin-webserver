6c2c7040c43acc87dcbd03ba7c21434b1043d077
Simon Nilsson <sim.nilsson@gmail.com>
2014-02-21 16:14:14 +0100

    Swedish example translation

---
+++ application/language/swedish/home_lang.php
@@ -0,0 +1,4 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+$lang['home_welcome'] = "Välkommen!";
+$lang['home_welcome_paragraph'] = "Lårem ipsum dålår sit nämen";
