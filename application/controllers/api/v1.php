d85e1a6a8b00ab26e345e71117651cb76cd30daa
Simon Nilsson <sim.nilsson@gmail.com>
2014-02-21 14:02:57 +0100

    Added a test for JSON api

---
+++ application/controllers/api/v1.php
@@ -0,0 +1,14 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class V1 extends CI_Controller {
+
+	public function index()
+	{
+		echo json_encode(array(
+				"error" => "not yet implemented"
+			));
+	}
+}
+
+/* End of file v1.php */
+/* Location: ./application/controllers/api/v1.php */
