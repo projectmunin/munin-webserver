1270d3beaaba072ecdb31d4380ac9e4dca664f82
Simon Nilsson <sim.nilsson@gmail.com>
2014-02-27 14:46:55 +0100

    Updated JQuery to 1.11. Replaced spaces with tabs

--- application/controllers/welcome.php
+++ application/controllers/welcome.php
@@ -2,31 +2,17 @@
 
 class Welcome extends CI_Controller {
 
-	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -  
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in 
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see http://codeigniter.com/user_guide/general/urls.html
-	 */
-	public function index()
+	function __construct()
 	{
+		parent::__construct();
 		$this->load->model('Lecture_note_library', '', true);
-
-
 		$this->load->helper('url');
 		$this->load->helper('language');
-
 		$this->lang->load('home');
+	}
 
+	public function index()
+	{
 		$navbar = $this->load->view('elements/navbar',array(),true);
 
 		$lecture_notes = $this->Lecture_note_library->get_latest_notes();
--- application/views/home.php
+++ application/views/home.php
@@ -1,47 +1,47 @@
 <!-- Main jumbotron for a primary marketing message or call to action -->
-    <div class="jumbotron">
-      <div class="container">
-        <h1><?=lang('home_welcome') ?></h1>
-        <p><?=lang('home_welcome_paragraph') ?></p>
-        <p><a class="btn btn-primary btn-lg">Learn more &raquo;</a></p>
-      </div>
-    </div>
+<div class="jumbotron">
+	<div class="container">
+		<h1><?=lang('home_welcome') ?></h1>
+		<p><?=lang('home_welcome_paragraph') ?></p>
+		<p><a class="btn btn-primary btn-lg">Learn more &raquo;</a></p>
+	</div>
+</div>
 
-    <div class="container">
-      <!-- Example row of columns -->
+<div class="container">
+	<!-- Example row of columns -->
 
-      <div class="row">
-        <div class="col-xs-12">
-          <h1>Latest lecture notes</h1>
-          <?php var_dump($lecture_notes); ?>
+	<div class="row">
+		<div class="col-xs-12">
+			<h1>Latest lecture notes</h1>
+			<?php var_dump($lecture_notes); ?>
 
-          <?php foreach($lecture_notes as $lecture_note): ?>
+			<?php foreach($lecture_notes as $lecture_note): ?>
 
-            <img class="lecture-note-img" src="<?=LECTURE_NOTES.$lecture_note->image ?>" />
-          <?php endforeach; ?>
-        </div>
-      </div>
-      <div class="row">
-        <div class="col-lg-4">
-          <h2>Heading</h2>
-          <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-          <p><a class="btn btn-default" href="#">View details &raquo;</a></p>
-        </div>
-        <div class="col-lg-4">
-          <h2>Heading</h2>
-          <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-          <p><a class="btn btn-default" href="#">View details &raquo;</a></p>
-       </div>
-        <div class="col-lg-4">
-          <h2>Heading</h2>
-          <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-          <p><a class="btn btn-default" href="#">View details &raquo;</a></p>
-        </div>
-      </div>
+				<img class="lecture-note-img" src="<?=LECTURE_NOTES.$lecture_note->image ?>" />
+			<?php endforeach; ?>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-lg-4">
+			<h2>Heading</h2>
+			<p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+			<p><a class="btn btn-default" href="#">View details &raquo;</a></p>
+		</div>
+		<div class="col-lg-4">
+			<h2>Heading</h2>
+			<p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+			<p><a class="btn btn-default" href="#">View details &raquo;</a></p>
+		</div>
+		<div class="col-lg-4">
+			<h2>Heading</h2>
+			<p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+			<p><a class="btn btn-default" href="#">View details &raquo;</a></p>
+		</div>
+	</div>
 
-      <hr>
+	<hr>
 
-      <footer>
-        <p>&copy; Company 2013</p>
-      </footer>
-    </div> <!-- /container -->
+	<footer>
+		<p>&copy; Company 2013</p>
+	</footer>
+		</div> <!-- /container -->
