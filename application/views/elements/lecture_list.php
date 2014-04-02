9a9fef0787420f07e325d12524dbe3f2b0705566
Simon Nilsson <sim.nilsson@gmail.com>
2014-04-02 22:41:15 +0200

    Lecture list styling

--- application/views/elements/lecture_list.php
+++ application/views/elements/lecture_list.php
@@ -1,12 +1,18 @@
 <?php foreach($lectures as $lecture): ?>
 	<div class="lecture-row">
-		<div class="lecture-thumb">
-			<a href="<?=lecture_url($lecture->id) ?>">
-				<img class="lecture-thumb-img" src="<?=base_url(LECTURE_NOTES.$lecture->lecture_notes[0]->image) ?>" />
-			</a>
+		<div class="col-sm-3 col-md-5">
+			<div class="lecture-thumb">
+				<a href="<?=lecture_url($lecture->id) ?>">
+					<img class="lecture-thumb-img" src="<?=base_url(LECTURE_NOTES.$lecture->lecture_notes[0]->image) ?>" />
+				</a>
+			</div>
 		</div>
-		<span class="lecture-row-time"><?=pretty_relative_time($lecture->startTime) ?></span>
-		<a class="lecture-row-course-name" href="<?=course_url($lecture->course->code,$lecture->course->period) ?>"><?=$lecture->course->name ?></span>
-		<a class="lecture-row-course-code" href="<?=course_url($lecture->course->code,$lecture->course->period) ?>"><?=$lecture->course->code ?> <?=$lecture->course->period ?></span>
+		<div class="col-sm-9 col-md-7">
+			<span class="lecture-row-time"><?=pretty_relative_time($lecture->startTime) ?> in <?=$lecture->lecture_hall_name ?></span>
+			<span class="lecture-row-course-code"><?=$lecture->course->code ?></span>
+			<span class="lecture-row-course-name"><?=$lecture->course->name ?></span>
+			<a href="<?=course_url($lecture->course->code,$lecture->course->period)?>" class="lecture-row-course-link"><span></span></a>
+			<a href="<?=lecture_url($lecture->id) ?>" class="lecture-row-meta"><?=count($lecture->lecture_notes) ?> recorded lecture notes</span>
+		</div>
 	</div>
 <?php endforeach; ?>
