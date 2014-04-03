cc539a06809c4ed0626b2b93e70e72105c1eddf1
Simon Nilsson <sim.nilsson@gmail.com>
2014-04-03 23:27:22 +0200

    Testing Disqus comments

--- application/views/lecture.php
+++ application/views/lecture.php
@@ -15,6 +15,33 @@
 					<div class="lecture-note-row">
 						<img src="<?=base_url(LECTURE_NOTES.$lecture_note->image) ?>" />
 						<span class="meta">recorded at <span class="date"><?=$lecture_note->time ?></span></span>
+						
+							<div id="disqus_thread"></div>
+						    <script type="text/javascript">
+						        /* * * CONFIGURATION VARIABLES: EDIT BEFORE PASTING INTO YOUR WEBPAGE * * */
+						        var disqus_shortname = 'projectmunin'; // required: replace example with your forum shortname
+						
+						        /* * * DON'T EDIT BELOW THIS LINE * * */
+						        (function() {
+						            var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+						            dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
+						            (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+						        })();
+						    </script>
+						    
+						    <script type="text/javascript">
+								/* * * CONFIGURATION VARIABLES: EDIT BEFORE PASTING INTO YOUR WEBPAGE * * */
+								var disqus_shortname = 'projectmunin'; // required: replace example with your forum shortname
+								
+								/* * * DON'T EDIT BELOW THIS LINE * * */
+								(function () {
+									var s = document.createElement('script'); s.async = true;
+									s.type = 'text/javascript';
+									s.src = '//' + disqus_shortname + '.disqus.com/count.js';
+									(document.getElementsByTagName('HEAD')[0] || document.getElementsByTagName('BODY')[0]).appendChild(s);
+								}());
+							</script>
+						    <noscript>Please enable JavaScript to view the <a href="http://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>    
 					</div>
 				<?php endforeach; ?>
 		</div>
