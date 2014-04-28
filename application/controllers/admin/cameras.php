a624f8b9373b205305f721d5fb32403778a06d0c
Simon Nilsson <sim.nilsson@gmail.com>
2014-04-28 11:03:25 +0200

    Added library to camera config command

--- application/controllers/admin/cameras.php
+++ application/controllers/admin/cameras.php
@@ -30,15 +30,18 @@ class Cameras extends MY_Controller {
 				{
 					if($this->input->post())
 					{
-						$command = "export DYLD_LIBRARY_PATH=''; cd /home/test/projectMunin/source/networkModule; java -Djava.awt.headless=true NetworkServerSendConfigs -r ".$this->input->post("name")." -l ".$this->input->post("location");
+						$command = "export DYLD_LIBRARY_PATH=''; cd /home/test/projectMunin/source/networkModule; java -cp .:mysql-connector-java-5.0.8-bin.jar -Djava.awt.headless=true NetworkServerSendConfigs -r ".$this->input->post("name")." -l ".$this->input->post("location");
 						
 						if($this->input->post("server_password"))
 						{
-							exec($command." -p ".$this->input->post("server_password"),$echo,$code);
+							$command .= " -p ".$this->input->post("server_password");
+							exec($command,$echo,$code);
+							log_message('debug', 'Running command: '.$command);
 						}
 						else
 						{
 							exec($command,$echo,$code);
+							log_message('debug', 'Running command: '.$command);
 						}
 						echo json_encode(array('exit_code' => $code));
 					}
